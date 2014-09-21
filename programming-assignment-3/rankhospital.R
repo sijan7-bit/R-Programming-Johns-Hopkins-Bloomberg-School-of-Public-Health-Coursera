6bc7b9e232b072aa36a00177d3dc8c69fb3fc3f2
UtkarshPathrabe <uapathrabe@gmail.com>
2014-09-21 18:19:44 +0530

    Final Commit
    The Code I Submitted

--- programming-assignment-3/rankhospital.R
+++ programming-assignment-3/rankhospital.R
@@ -1,43 +1,45 @@
-rankhospital <- function(state, outcome, num) {
-    ## Read outcome data
-    data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-    data <- data[c(2, 7, 11, 17, 23)]
-    names(data)[1] <- "name"
-    names(data)[2] <- "state"
-    names(data)[3] <- "heart attack"
-    names(data)[4] <- "heart failure"
-    names(data)[5] <- "pneumonia"
-
-    ## Validate the outcome string
-    outcomes = c("heart attack", "heart failure", "pneumonia")
-    if( outcome %in% outcomes == FALSE ) stop("invalid outcome")
-
-    ## Validate the state string
-    states <- data[, 2]
-    states <- unique(states)
-    if( state %in% states == FALSE ) stop("invalid state")
-    
-    ## Validate the num value
-    if( num != "best" && num != "worst" && num%%1 != 0 ) stop("invalid num")
-
-    ## Grab only rows with our state value    
-    data <- data[data$state==state & data[outcome] != 'Not Available', ]
-
-    ## Order the data
-    data[outcome] <- as.data.frame(sapply(data[outcome], as.numeric))
-    data <- data[order(data$name, decreasing = FALSE), ]
-    data <- data[order(data[outcome], decreasing = FALSE), ]
-
-    ## Process the num argument
-    vals <- data[, outcome]
-    if( num == "best" ) {
-        rowNum <- which.min(vals)
-    } else if( num == "worst" ) {
-        rowNum <- which.max(vals)
-    } else {
-        rowNum <- num
-    }
-
-    ## Return hospital name in that state with lowest 30-day death rate
-    data[rowNum, ]$name
+rankhospital <- function(state, outcome, num = "best") {
+        ## Reading the outcome data
+        data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+        ## Selecting the columns that are reqiured and naming them
+        data <- data[c(2, 7, 11, 17, 23)]
+        names(data)[1] <- "name"
+        names(data)[2] <- "state"
+        names(data)[3] <- "heart attack"
+        names(data)[4] <- "heart failure"
+        names(data)[5] <- "pneumonia"
+        
+        ## Validating the outcome string
+        outcomes = c("heart attack", "heart failure", "pneumonia")
+        if(outcome %in% outcomes == FALSE) stop("invalid outcome")
+        
+        ## Validating the state string
+        states <- data[, 2]
+        states <- unique(states)
+        if(state %in% states == FALSE) stop("invalid state")
+        
+        ## Validating the num value
+        if(num != "best" && num != "worst" && num%%1 != 0) stop("invalid num")
+        
+        ## Grab only the rows with matches the reqiured state value    
+        data <- data[data$state==state & data[outcome] != 'Not Available', ]
+        
+        ## Ordering the data in ascending order, first according to the names 
+        ## column and then according to their ranks for the specific outcome column
+        data[outcome] <- as.data.frame(sapply(data[outcome], as.numeric))
+        data <- data[order(data$name, decreasing = FALSE), ]
+        data <- data[order(data[outcome], decreasing = FALSE), ]
+        
+        ## Processing the num argument for various conditions
+        vals <- data[, outcome]
+        if(num == "best"){
+                rowNum <- which.min(vals)
+        }else if(num == "worst"){
+                rowNum <- which.max(vals)
+        }else{
+                rowNum <- num
+        }
+        
+        ## Return hospital name in that state with lowest 30-day death rate
+        data[rowNum, ]$name
 }
